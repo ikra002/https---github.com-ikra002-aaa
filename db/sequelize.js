bd35e4f590ab721539f3eb214661c7d797263747
ikra002 <164364879+ikra002@users.noreply.github.com>
2024-04-01 23:28:30 +0400

    sequelize.js

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,14 +1,21 @@
+import { config } from 'dotenv';
 import {Sequelize} from 'sequelize'
 
-export default new Sequelize(
+//export default new Sequelize(
+const sequelize = new Sequelize(
     process.env.DB_NAME,"shop",// база данных
     process.env.DB_USER,"postgres",// пользователь
     process.env.DB_PASS,"1235", // пароль
     {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        dialect: 'postgres',        
+        dialectOptions: {
+            host: "localhost", //process.env.DB_HOST
+            port: '2000'//process.env.DB_PORT
+          }
+        // host: process.env.DB_HOST,
+        // port: process.env.DB_PORT
+        
     }
 )
-
-module.exports = config;
+Sequelize.afterConnect();
+module.exports = config
